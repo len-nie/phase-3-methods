d48b9b3fc0524e42fe23f832ef4bfd9dc08c0b34
Lennie <lennie.mbotela@student.moringaschool.com>
2023-06-20 02:09:10 +0300

    completed lab

--- methods.rb
+++ methods.rb
@@ -1,1 +1,41 @@
 # Your code here!
+
+def greet_programmer 
+    puts "Hello, programmer!"
+end
+
+greet_programmer
+
+def greet(name)
+        puts "Hello, #{name}!"
+end
+
+greet("Naureen")   
+
+def greet_with_default(name = "programmer")
+    puts "Hello, #{name}!"
+  end
+
+greet_with_default("Sunny")
+greet_with_default()
+
+def add (number1, number2)
+    number1 + number2
+end
+
+result = add(1, 2)
+puts result
+
+def halve(number)
+    return nil unless number.is_a?(Integer)
+
+# this part made me sweat wueh
+
+  number / 2
+end
+
+result = halve(100)
+puts result
+
+
+  
